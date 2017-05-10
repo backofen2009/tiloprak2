de28dfda5bd4ed217d5df3378dfc7b10ff8cd7c5
RBEGamer <marcel.ochsendorf@gmail.com>
2017-05-10 15:45:22 +0200

    added files not complete working

--- P2/prak2.pl
+++ P2/prak2.pl
@@ -1,44 +1,57 @@
-app(nil,Xs,Xs).
-app(list(X,X1s),Ys,list(X,X2s)) :- app(X1s,Ys,X2s).
-add(o,Y,Y).
-add(s(X),Y,s(R)). :- add(X,Y,R).
-natSymb(o).
-natSymb(s(X)) :- natSymb(X).
 
-%linListe(Xs)
+app(nil,Ys,Ys).
+app(list(X,Y),A,list(X,S)) :- app(Y,A,S).
+
+%add(nil,nil,nil).
+%add(o,Y,Y).
+%add(s(X),Y,s(R)). :- add(X,Y,R).
+
+
+% A)
 linListe(nil).
-linListe(list(X,Xs)).
-
-%member(X,Ys) : Ys enthält das Element X.
-prakmember(nil,nil).
-prakmember(X,Ys) :- linListe(list(X,Ys)), app(nil,X,Ys).
-
-%infix(Xs,Ys) : Ys enthält die Liste Xs.
-infix(nil,nil).
-infix(Xs,Ys) :- linListe(list(X,Ys)), app(nil,Xs,Ys).
+linListe(list(X,Xs)) :- linListe(Xs).
+% B 1)
+member(nil,nil).
+member(X,list(X,Ys)).
+member(X,list(Y,Ys)) :- member(X,Ys), X \= Y.
 
 
+praefix(nil, Ys).
+praefix(list(X,Xs),list(X,Ys)) :- praefix(Xs,Ys).
+
+%B 2)
+infix(Infix,Liste) :-app(_,Infix,R), app(R,_,Liste).
 
 
+tree(nil).
+tree(value,X,Y).
 
-
-%n(X,Lb,Rb).
 binbaum(nil).
-binbaum(n(X,Lb,Rb)) :- binbaum(Lb), binbaum(Rb).
-
-root(n(W,Lb,Rb),b).
-left(n(W,Lb,Rb),Lb).
-right(n(W,Lb,Rb),Rb).
-
+binbaum(tree(V,X,Y)) :- binnbaum(X), binbaum(Y).
 
 %construct(Root,Lb,Rb,Xneub) : Xneub ist der Baum mit
-%- Wurzelbeschriftung Root,
+%- baum wurzel root Root,
 %- linkem Teilbaum Lb und
 %- rechtem Teilbaum Rb.
-
+% 2 B)
+%leere liste kann nicht construct werden
 construct(nil,nil,nil,nil).
-construct(Root,Lb,Rb,n(Root,Lb,Rb)):- binbaum(Lb), binbaum(Rb).
+%werte in den root einfügen zusammenfügen
+construct(Root,Lb,Rb,tree(Root,Lb,Rb)):- binbaum(Lb), binbaum(Rb).
 
+
+add(X,o,X).
+add(X,s(Y),R) :- add(s(X), Y, R).
+
+% 2 A)
 %knotenanz(Xb,N) : N (nat. Zahl in symbolischer Darstellung) ist die Anzahl der Knoten des Baumes Xb.
-knotenanz(nil,o).
-knotenanz(n(X,Lb,Rb),s(N)) :- knotenanz(Rb,R2), knotenanz(Lb,R1), add(R2,R1,N), natSymb(N).
+knotenanz(nil,0).
+knotenanz(tree(V,Lb,Rb),N) :- knotenanz(Lb,Cl), knotenanz(Rb,Cr), N is 1 + Cl + Cr.
+
+
+
+%rev(Xs,Ys).
+%leeres liste ist auch leer
+rev(nil,nil).
+% das was man vorne gerausgenommen hat hinten wieder einfügen
+rev(list(X,Xs),Ys) :- rev(Xs,Rs), app(Rs,list(X,nil),Ys).
